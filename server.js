e61554f3eeffee84207f99e273ce5da655915fdf
Oleg0009 <42346617+Oleg0009@users.noreply.github.com>
2023-09-21 14:48:06 +0300

    update db

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ limit:"10mb", extended:false }))
 
 
 //DB Connection
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect('mongodb+srv://user:LdrbwtYcyWzJv6id@cluster0.ojwdqvv.mongodb.net/?retryWrites=true&w=majority')
 const db =mongoose.connection
 db.on('error',error=>console.error(error))
 db.once('open',()=>{console.log('Coneccted');})
