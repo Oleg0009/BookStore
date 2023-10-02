10b5c76381e250001b3bdb3a9b8c8175e79baa41
Oleg0009 <42346617+Oleg0009@users.noreply.github.com>
2023-10-02 16:25:33 +0300

    finish styles

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,17 +1,26 @@
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
+const rootStyles = window.getComputedStyle(document.documentElement)
+const ready = () =>{
+  console.log('ready')
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width'))
+  const aspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ration')) 
+  const coverHeight = coverWidth / aspectRatio; 
+  
   FilePond.registerPlugin(FilePondPluginImagePreview);
   FilePond.registerPlugin(FilePondPluginImageResize);
   FilePond.registerPlugin(FilePondPluginFileEncode);
   
   FilePond.setOptions({
-    stylePanelAspectRatio:150/100,
-    imageResizeTargetWidth:100,
-    imageResizeTargetHeight:150
+    stylePanelAspectRatio:1 / aspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
   })
 
   FilePond.parse(document.body);
-});
+}
+
+if(rootStyles.getPropertyValue('--book-cover-width') != null  && rootStyles.getPropertyValue('--book-cover-width') !== ''){
+  ready()
+}else{
+  document.getElementById('mainCss').addEventListener('load',ready)
+}
