0622319a7e63ebfc51ccfaf340a4d8456d5b0b8c
Oleg0009 <42346617+Oleg0009@users.noreply.github.com>
2023-10-06 16:43:00 +0300

    update author page

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -25,3 +25,45 @@ if(rootStyles.getPropertyValue('--book-cover-width') != null  && rootStyles.getP
 }else{
   document.getElementById('mainCss').addEventListener('load',ready)
 }
+
+
+const productCards = document.querySelectorAll('.author-card');
+
+function showCardsOneByOne(cards) {
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.style.animation = 'slideIn 0.5s ease-in-out';
+      card.style.animationFillMode = 'forwards';
+      card.style.animationPlayState = 'running';
+    }, index * 500); // Adjust the delay (in milliseconds) as needed
+  });
+}
+
+// Call the function to start the animation
+showCardsOneByOne(productCards);
+
+// JavaScript for lazy loading (fallback)
+const lazyImages = document.querySelectorAll('.lazy');
+
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-src');
+        img.setAttribute('src', src);
+        img.classList.remove('lazy');
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+lazyImages.forEach(lazyLoad);
+
+
+
+
+
